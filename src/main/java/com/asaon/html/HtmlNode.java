d5bd4df3fa309662cb6e1cb973540d0407c1c753
Matthias Perktold <tias251@gmail.com>
2021-09-07 08:02:50 +0200

    Add HtmlNode.build(UnaryOperator)
    Also use static imports for node factories in tests

--- src/main/java/com/asaon/html/HtmlNode.java
+++ src/main/java/com/asaon/html/HtmlNode.java
@@ -2,27 +2,35 @@ package com.asaon.html;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.UnaryOperator;
 
 public sealed interface HtmlNode {
 
-	record Text(String content) implements HtmlNode {}
+	record Text(String content) implements HtmlNode {
+		@Override
+		public <B extends HtmlBuilder<B>> B addTo(HtmlBuilder<B> builder) {
+			return builder.text(content);
+		}
+	}
 
 	record Element(String tag, Map<String, String> attrs, List<? extends HtmlNode> content) implements HtmlNode {
 		public Element {
 			attrs = Map.copyOf(attrs);
 			content = List.copyOf(content);
 		}
+
+		@Override
+		public <B extends HtmlBuilder<B>> B addTo(HtmlBuilder<B> builder) {
+			var inner = builder.tag(tag, attrs);
+			content.forEach(c -> c.addTo(builder));
+			return inner.tagEnd(tag);
+		}
 	}
 
-	default <B extends HtmlBuilder<?>> B addTo(B builder) {
-		if (this instanceof Text t)
-			builder.text(t.content());
-		else if (this instanceof Element e) {
-			var inner = builder.tag(e.tag(), e.attrs());
-			e.content().forEach(c -> c.addTo(builder));
-			inner.tagEnd(e.tag());
-		}
-		return builder;
+	<B extends HtmlBuilder<B>> B addTo(HtmlBuilder<B> builder);
+
+	static List<HtmlNode> build(UnaryOperator<HtmlBuilder.Root<List<HtmlNode>>> expr) {
+		return builder().include(expr).build();
 	}
 
 	static HtmlBuilder.Root<List<HtmlNode>> builder() {
